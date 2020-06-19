38539a2944b572db6da9fffc248f4e67fa4fa29c
Juan <juanruizh2406@gmail.com>
2020-06-18 19:15:29 -0600

    venta (agregar producto a tblIntermedia)

---
+++ src/app/models/sale_product.ts
@@ -0,0 +1,11 @@
+export class Sale_Product{
+    constructor(
+      public id:number,
+      public quantity:number,
+      public totalPrice:number,
+      public created_at:any,
+      public updated_at:any,
+      public idSale:any,
+      public idProduct:any
+    ){}
+  }
